3036e9c4c90b7b4bdf5193ad3cd8af6e2eb670d3
Ismail D <orkinosai@protonmail.com>
2025-06-10 10:06:43 +0000

    changes

--- iosdemo/ViewController.swift
+++ iosdemo/ViewController.swift
@@ -1,76 +1,42 @@
-//
-//  ViewController.swift
-//  HelloWorldProject
-//
-//  Created by Cora Xing on 9/12/18.
-//  Copyright © 2018 Cora Yichen Xing. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textLabel: UILabel!
-    
-    @IBOutlet weak var textField: UITextField!
-    
-    var initBackgroundColor: UIColor!
-    var initTextColor: UIColor!
-    
+    // MARK: - IBOutlets
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var connectButton: UIButton!
+
+    // Demo credentials (replace with your actual test values)
+    let vpnServer = "demo.vpnserver.com"
+    let vpnPort = 443
+    let vpnHub = "DEMOHUB"
+    let vpnUsername = "demouser"
+    let vpnPassword = "demopass"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // Save initial background and text colors
-        initBackgroundColor = view.backgroundColor
-        initTextColor = textLabel.textColor
+        statusLabel.text = "Ready to connect"
+        statusLabel.textAlignment = .center
+        connectButton.setTitle("Connect", for: .normal)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - IBActions
+    @IBAction func connectTapped(_ sender: UIButton) {
+        statusLabel.text = "Connecting..."
+        print("Connect button tapped! Calling VPN wrapper...")
 
-    }
+        let result = SoftEtherVPNWrapper.connectToServer(
+            vpnServer,
+            port: vpnPort,
+            hub: vpnHub,
+            username: vpnUsername,
+            password: vpnPassword
+        )
 
-    @IBAction func didTapChangeTextColorButton(_ sender: Any) {
-        // Print hello and change text hello text color
-        
-        print("Hello")
-        if textLabel.textColor == UIColor.orange {
-            textLabel.textColor = initTextColor
+        if result {
+            statusLabel.text = "Connected!"
         } else {
-            textLabel.textColor = UIColor.orange
+            statusLabel.text = "Failed."
         }
-        
     }
-    
-    @IBAction func didTapChangeViewColorButton(_ sender: Any) {
-        // Change background color of screen
-        
-        view.backgroundColor = UIColor(displayP3Red: (42/255.0), green: (33/255.0), blue: (78/255.0), alpha: 1)
-    }
-    
-    @IBAction func didTapChangeTextButton(_ sender: Any) {
-        // Change text label to say something else
-        
-        textLabel.text = textField.text
-        textField.text = ""
-        view.endEditing(true)
-    }
-    
-    @IBAction func didTapSayGoodbyeButton(_ sender: Any) {
-        // Change text label to say goodbye
-        
-        textLabel.text = "Goodbye! 👋"
-    }
-    
-    @IBAction func OnReset(_ sender: Any) {
-        // Reset to initial text and colors
-        
-        textLabel.text = "Hello from Cora!"
-        view.backgroundColor = initBackgroundColor
-        textLabel.textColor = initTextColor
-    }
-    
 }
-
